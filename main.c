6e547040899f33c52f2d137ef062dd5bd3aebdde
Nicholas T. <ntoutges@gmail.com>
2026-04-07 19:53:50 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "cmd.h"
+
+cmd_t myCmd;
+
+uint8_t bufBuf[20];
+cmd_entry_t entryBuf[10];
+
+int main() {
+    myCmd = cmd(entryBuf, 10, bufBuf, sizeof(bufBuf), '!');
+
+    char* command = "!hello world a\n";
+    for (int i = 0; i < strlen(command); i++) {
+        cmd_recv(&myCmd, command[i]);
+    }
+
+    printf("0: %s\n", cmd_ogets(&myCmd, 0, "/"));
+    printf("1: %s\n", cmd_ogets(&myCmd, 1, "/"));
+    printf("2: %s\n", cmd_ogets(&myCmd, 2, "/"));
+
+    printf("\n----\n\n");
+
+    char* command2 = "!hello world --a b c d\n";
+    for (int i = 0; i < strlen(command2); i++) {
+        cmd_recv(&myCmd, command2[i]);
+    }
+
+    printf("0: %s\n", cmd_ogets(&myCmd, 0, "/"));
+    printf("1: %s\n", cmd_ogets(&myCmd, 1, "/"));
+    printf("2: %s\n", cmd_ogets(&myCmd, 2, "/"));
+    printf("3: %s\n", cmd_ogets(&myCmd, 3, "/"));
+    printf("4: %s\n", cmd_ogets(&myCmd, 4, "/"));
+    printf("5: %s\n", cmd_ogets(&myCmd, 5, "/"));
+    printf("a: %s\n", cmd_ugets(&myCmd, "a", "/"));
+
+    printf("\n----\n\n");
+
+    char* command3 = "!hello world this is a command overrun that is way too long!\n";
+    for (int i = 0; i < strlen(command3); i++) {
+        cmd_recv(&myCmd, command3[i]);
+    }
+
+    // Print out bufBuf
+    for (int i = 0; i < sizeof(bufBuf); i++) {
+        char c = bufBuf[i];
+
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+            printf("%c ", c);
+        } else if (c == 0x00) {
+            printf("/ ");
+        } else {
+            printf("%02X ", c);
+        }
+    }
+    printf("\n");
+
+    printf("0: %s\n", cmd_ogets(&myCmd, 0, "/"));
+    printf("1: %s\n", cmd_ogets(&myCmd, 1, "/"));
+    printf("2: %s\n", cmd_ogets(&myCmd, 2, "/"));
+    printf("3: %s\n", cmd_ogets(&myCmd, 3, "/"));
+    printf("4: %s\n", cmd_ogets(&myCmd, 4, "/"));
+
+    return 0;
+}
